4a8bcc172a7800391a36a4e351085fc101ccf709
Carl Fink <cafink@gmail.com>
2025-04-25 09:51:08 -0500

    Add button to remove holiday

--- src/components/HolidayList.tsx
+++ src/components/HolidayList.tsx
@@ -1,14 +1,25 @@
+import { useState, MouseEvent } from "react";
+
 interface Props {
   holidays: Object[];
 }
 
 const HolidayList = ({ holidays }: Props) => {
+
+  const [holidayList, setHolidayList] = useState(holidays);
+
+  const removeHoliday = (id: string) => {
+    const updatedList = holidayList.filter((item) => item.date + item.name !== id);
+    setHolidayList(updatedList);
+  }
+
   return (
-    <ul>
-      { holidays.map((holiday) => (
-        <li key={ holiday.date + holiday.name }>
+    <ul className="list-group">
+      { holidayList.map((holiday, index) => (
+        <li className="list-group-item" key={ holiday.date + holiday.name }>
           { new Date(holiday.date).toLocaleDateString() }:&nbsp;
-          <strong>{ holiday.name }</strong>
+          <strong>{ holiday.name }</strong>&nbsp;
+          <span onClick={() => removeHoliday(holiday.date + holiday.name)}>&#10060;</span>
         </li>
       )) }
     </ul>
